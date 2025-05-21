b8ddad225ba432f8680f23bdec4f94074c4a6d6e
Zinful <zinful@tutamail.com>
2025-05-21 04:01:03 -0400

    Update updater.py

--- Universal Tools/updater.py
+++ Universal Tools/updater.py
@@ -1,11 +1,36 @@
 #!/usr/bin/env python3
 
 import requests
+import time
 link0 = "https://raw.githubusercontent.com/Zin-ful/zin-minimal-suite/refs/heads/main/Universal%20Tools/updater.py"
 link1 = "https://raw.githubusercontent.com/Zin-ful/zin-minimal-suite/refs/heads/main/zweather/weathertool.py"
 link2 = "https://raw.githubusercontent.com/Zin-ful/zin-minimal-suite/refs/heads/main/zweather/weathertool-backgroundproc.py"
 link3 = "https://raw.githubusercontent.com/Zin-ful/zin-minimal-suite/refs/heads/main/zweather/linux-weathertool.py"
-print("Tool Version: 1.4\nWhat would you like to update?\n\n0. Updater\n1. Weather Tool\n2. Weather Tool (Linux)\n3. Weather Tool Web")
+link4 = "https://raw.githubusercontent.com/Zin-ful/zin-minimal-suite/refs/heads/main/zbrowse/zbrowse.py"
+link5 = "https://raw.githubusercontent.com/Zin-ful/zin-minimal-suite/refs/heads/main/ztalk/ztext/message_intelligent.py"
+link6 = "https://raw.githubusercontent.com/Zin-ful/zin-minimal-suite/refs/heads/main/ztalk/ztext_srvr/message_server.py"
+link7 = "https://raw.githubusercontent.com/Zin-ful/zin-minimal-suite/refs/heads/main/zfile/client/client.py"
+link8 = "https://raw.githubusercontent.com/Zin-ful/zin-minimal-suite/refs/heads/main/zfile/server/server.py"
+link9 = "https://raw.githubusercontent.com/Zin-ful/zin-minimal-suite/refs/heads/main/zfile/server/handle_cmd.py"
+link10 = "https://raw.githubusercontent.com/Zin-ful/zin-minimal-suite/refs/heads/main/zapp/server/zapp_server.py"
+link11 = "https://raw.githubusercontent.com/Zin-ful/zin-minimal-suite/refs/heads/main/zapp/client/zget.py"
+link12 = "https://raw.githubusercontent.com/Zin-ful/zin-minimal-suite/refs/heads/main/Universal%20Tools/Uniconfig.py"
+link13 = "https://raw.githubusercontent.com/Zin-ful/zin-minimal-suite/refs/heads/main/Universal%20Tools/Unihelp.py"
+numbertoname = {"0": "Update Tool", "1": "Weather Tool", "2": "Weather Tool (Background)", "3": "Weather Tool (linux)", "4": "File Browser", "5": "Messenger", "6": "Messenger Server", "7": "Cloud Storage Client", "8": "Cloud Storage Server (Depends on handler)", "9": "Handler", "10":"Application Server", "11": "Application Retreiver", "12": "Universal Helper", "13": "Universal Configuration Tool"}
+apps_dict = {"Update Tool": link0, "Weather Tool": link1, 
+	     "Weather Tool (Background)": link2, "Weather Tool (linux)": link3, 
+	     "File Browser": link4, "Messenger": link5, "Messenger Server": link6, 
+	     "Cloud Storage Client": link7, "Cloud Storage Server (Depends on handler)": link8, 
+	     "Handler": link9, "Application Server": link10, "Application Retreiver": link11,
+	     "Universal Helper": link12, "Universal Configuration Tool": link13}
+
+print("Tool Version: 1.6\nWelcome to the emergency application tool. Or E.A.T.\nThis tool has been called because A: You are using it directly\nB: The application installer has failed to connect to a server\nOr C: The application installer does not exist.\nLets go ahead and look at your options for install.\nWait about 10 seconds while i grab those.")
+time.sleep(10)
+m = 0
+for name, trash in apps_dict:
+	print(f"{m}. {name}")
+print("What would you like to retrieve?")
+
 def updateme(link, name):
 	response = requests.get(link)
 	errors = response.raise_for_status()
@@ -25,18 +50,8 @@ while True:
 	inp = input(">>> ")
 	if "exit" in inp:
 		exit()
-	if "0" in inp or "upd" in inp or "Upd" in inp or "uPD" in inp:
-		updateme(link0, "updater.py")
-		break
-	elif "1" in inp or inp == "Weather Tool" or inp == "weather tool" or inp == "WeatherTool" or inp == "weathertool":
-		updateme(link1, "weathertool.py")
-		break
-	elif "3" in inp or "web" in inp or "Web" in inp or "WEB" in inp or "wEB" in inp:
-		updateme(link2, "wt-background.py")
-		break
-	elif "2" in inp or "li" in inp or "Li" in inp or "lI" in inp or "wEB" in inp:
-		updateme(link3, "wt")
-		break
-	else:
-		print("Sorry but that isnt an option")
-		continue
+	for i, name in numbers_list:
+		if inp in i:
+			inp = int(inp)
+	
+
