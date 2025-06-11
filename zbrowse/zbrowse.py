b6bd96af76a4e676f3c6b9763ff4bc560645adbc
Zinful <zinful@tutamail.com>
2025-06-11 16:11:17 -0400

    Add files via upload

--- zbrowse/zbrowse.py
+++ zbrowse/zbrowse.py
@@ -14,6 +14,8 @@ pos = 0
 sx = 0
 lvl = 0
 origin = 0
+num = 0
+
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 files_in_path = os.listdir(cur_dir)
 SELECT = None
@@ -24,6 +26,8 @@ conf_path = "/etc/zbrowse"
 
 if "zbrowse" not in os.listdir("/etc"):
     os.makedirs(conf_path, exist_ok=True)
+if "trash" not in os.listdir(conf_path):
+    os.makedirs(f"{conf_path}/trash", exist_ok=True)
 
 def main(stdscr):
     global SELECT, height, width
@@ -36,29 +40,32 @@ def main(stdscr):
     menu_win = curses.newwin(height - 1, width - 10, 0, 10)
     files_win.timeout(10)
     win = {"main": stdscr, "files": files_win, "menu": menu_win}
-    list_file(win, 0.02)
+    list_file(win, 0.02, 1)
     inps(win)
 
-def list_file(win, delay):
-    global origin, files_in_path, pos, sx, lvl
+def list_file(win, delay, rst):
+    global origin, files_in_path, pos, sx, lvl, num
     time.sleep(0.1)
     win["files"].clear()
     files_in_path = os.listdir(cur_dir)
+    if rst:
+        pos = 0
+        lvl = 0
+        num = 0
+    sx = 0
     i = 0
     y = 0
     x = 0
-    sx = 0
-    pos = 0
-    lvl = 0
     for item in files_in_path.copy():
         if item[0] == "." and hidden:
             files_in_path.remove(item)
-
     for item in files_in_path:
+        if num * (height - 4) + i >= len(files_in_path):
+            break
         if x >= width - 5:
             return
         else:
-            win["files"].addstr(y, x, item, curses.COLOR_WHITE)
+            win["files"].addstr(y, x, files_in_path[num * (height - 4) + i], curses.COLOR_WHITE)
         i += 1
         y += 1
         if y >= height - height // 10:
@@ -82,7 +89,7 @@ def inps(win):
             continue
         if key == ord("a") or key == ord("d"):
             cur_dir = move(key)
-            list_file(win, 0.002)
+            list_file(win, 0.002, 1)
             win["files"].refresh()
         elif key == ord("w") or key == ord("s"):
             select(win, key, "files", files_in_path, None)
@@ -112,8 +119,7 @@ def menu(win):
     win["menu"].addstr(origin + 1 - offset, sx + cache, "Paste ", curses.COLOR_WHITE)
     win["menu"].addstr(origin + 2 - offset, sx + cache, "Delete ", curses.COLOR_WHITE)
     win["menu"].addstr(origin + 3 - offset, sx + cache, "Edit ", curses.COLOR_WHITE)
-    win["menu"].addstr(origin + 4 - offset, sx + cache, "Read ", curses.COLOR_WHITE)
-    options = ["Copy ", "Paste ", "Delete ", "Edit ", "Read "]
+    options = ["Copy ", "Paste ", "Delete ", "Edit "]
     x = 0
     while True:
         if x:
@@ -121,7 +127,6 @@ def menu(win):
             win["menu"].addstr(origin + 1 - offset, sx + cache, "      ", curses.COLOR_WHITE)
             win["menu"].addstr(origin + 2 - offset, sx + cache, "       ", curses.COLOR_WHITE)
             win["menu"].addstr(origin + 3 - offset, sx + cache, "     ", curses.COLOR_WHITE)
-            win["menu"].addstr(origin + 4 - offset, sx + cache , "     ", curses.COLOR_WHITE)
             win["menu"].refresh()
             pos = origin
             return
@@ -139,7 +144,7 @@ def menu(win):
                 x = 1
 
 def select(win, inp, subwin, word_list, xoffset):
-    global pos, sx, lvl
+    global pos, sx, lvl, num
     if not xoffset:
         xoffset = 0
     if not word_list or len(word_list) < 0:
@@ -153,14 +158,25 @@ def select(win, inp, subwin, word_list, xoffset):
         offset = -1
         pos -= 1
     prevlen = ""
-    if pos >= height - 6 and len(word_list) > height - 6 and subwin == "files":
+    if pos >= height - 4 and len(word_list) > height - 4 and subwin == "files":
         pos = 0
         lvl += 1
         sx += width // 3
+        if sx >= width - 3:
+            if sx // width == 0:
+                num += 3
+            else:
+                num += 3
+            list_file(win, 0.002, 0)
     elif pos == -1 and lvl >= 1 and subwin == "files":
         sx -= width // 3
-        pos = height - 4
+        pos = height - 5
         lvl -= 1
+        if sx < 0 and num:
+            num -= 3
+            list_file(win, 0.002, 0)
+            sx = (width - width // 3) - 2
+
     if pos <= 0:
         offset = 0
         pos = 0
@@ -170,20 +186,24 @@ def select(win, inp, subwin, word_list, xoffset):
     for i in word_list[pos]:
         prevlen += " "
     if lvl:
-        lvl_offset = (height - 6) * lvl
+        lvl_offset = (height - 4) * lvl
     else:
         lvl_offset = 0
     if pos + lvl_offset >= len(files_in_path) and subwin == "files":
         pos -= 1
+    final_pos = origin + pos - offset
     if subwin == "files":
         if pos > -1:
-            win[subwin].addstr(origin + pos - offset, sx + xoffset, prevlen, curses.COLOR_BLACK)
-            win[subwin].addstr(origin + pos - offset, sx + xoffset, word_list[pos - offset + lvl_offset], curses.COLOR_BLACK)
+            if  final_pos >= height - 4 or final_pos >= len(files_in_path):
+                pass
+            else:
+                win[subwin].addstr(final_pos, sx + xoffset, prevlen, curses.COLOR_BLACK)
+                win[subwin].addstr(final_pos, sx + xoffset, word_list[pos - offset + lvl_offset], curses.COLOR_BLACK)
         win[subwin].addstr(origin + pos, sx + xoffset, word_list[pos + lvl_offset], SELECT)
     else:
         if pos > -1:
-            win[subwin].addstr(origin + pos - offset, sx + xoffset, prevlen, curses.COLOR_BLACK)
-            win[subwin].addstr(origin + pos - offset, sx + xoffset, word_list[pos - offset], curses.COLOR_BLACK)
+            win[subwin].addstr(final_pos, sx + xoffset, prevlen, curses.COLOR_BLACK)
+            win[subwin].addstr(final_pos, sx + xoffset, word_list[pos - offset], curses.COLOR_BLACK)
         win[subwin].addstr(origin + pos, sx + xoffset, word_list[pos], SELECT)
     win[subwin].refresh()
 
@@ -199,7 +219,7 @@ def edit(win):
         time.sleep(0.2)
         win["main"].clear()
         win["main"].refresh()
-        list_file(win, 0.02)
+        list_file(win, 0.02, 1)
         inps(win)
 def copy(win):
     global data_name, data_path
@@ -214,7 +234,6 @@ def paste(win):
         if item == file_name:
             file_name = f"{data_name.replace('_cpy{count-1}', '')}_cpy{count}"
             count += 1
-
     with open(data_path, "rb") as filer:
         filer.seek(0, 2)
         file_size = filer.tell()
@@ -225,23 +244,46 @@ def paste(win):
                 if not chunk:
                     break
                 filew.write(chunk)
-    list_file(win, 0.002)
+    list_file(win, 0.002, 1)
 
-def rd(win):
-    return
-
+def cache_file():
+    count = 1
+    file_name = data_name
+    for item in files_in_path:
+        if item == file_name:
+            file_name = f"{data_name.replace('_cpy{count-1}', '')}_cpy{count}"
+            count += 1
+    with open(data_path, "rb") as filer:
+        filer.seek(0, 2)
+        file_size = filer.tell()
+        filer.seek(0)
+        with open(f"{conf_path}/trash/{file_name}", "wb") as filew:
+            while True:
+                chunk = filer.read(chunk_size)
+                if not chunk:
+                    break
+                filew.write(chunk)
 def delete(win):
-    return
-
-actions = {"Copy ": copy, "Paste ": paste, "Delete ": delete, "Edit ": edit, "Read ": rd}
+    copy(win)
+    cache_file()
+    os.remove(f"{cur_dir}/{files_in_path[origin]}")
+    list_file(win, 0.002, 1)
+actions = {"Copy ": copy, "Paste ": paste, "Delete ": delete, "Edit ": edit}
 
 def move(inp):
     rev_dir = os.path.dirname(f"{cur_dir}..")
     if files_in_path or len(files_in_path) > 0:
         if cur_dir == "/":
-            forw_dir = cur_dir + files_in_path[pos]
+            forw_dir = cur_dir + files_in_path[num * sx + pos]
         else:
-            forw_dir = cur_dir + "/" + files_in_path[pos]
+            if not num and sx:
+                calc = (lvl * (height - 4)) + pos
+                forw_dir = cur_dir + "/" + files_in_path[calc]
+                log(f"CHECK ME {cur_dir + '/' + files_in_path[calc]}")
+            else:
+                calc = (lvl * (height - 4)) + pos
+                forw_dir = cur_dir + "/" + files_in_path[calc]
+                log(f"CHECK ME {cur_dir + '/' + files_in_path[calc]} {calc}")
     if inp == ord("a"):
         if cur_dir == "/":
             log(f"root: {cur_dir}")
@@ -260,7 +302,6 @@ def move(inp):
             log(f"forward: {forw_dir}")
         except:
             return cur_dir
-            edit(forw_dir)
         log(f"forward: {forw_dir}")
         return forw_dir
 
