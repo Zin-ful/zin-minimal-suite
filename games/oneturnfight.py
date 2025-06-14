07b17e0f7d6841be11f76dcd7691f6746d1d07ca
Zinful <zinful@tutamail.com>
2025-06-14 05:24:02 -0400

    Add files via upload

--- games/oneturnfight.py
+++ games/oneturnfight.py
@@ -12,12 +12,14 @@ username = ""
 password = ""
 ipaddr = None
 port = None
-
+auto = 1
 auth = {"user": username, "pass": password, "ip": ipaddr, "port": port}
+
+botnames = ["john doe", "vinny krob", "bill starch", "tim stronk", "jim giggs", "shik steam", "devin"]
 
 level = 0
 exp = 0
-expreq = 0
+expreq = 20
 health = 20
 attack = 0
 defense = 0
@@ -26,12 +28,27 @@ points = 0
 
 done = 0
 
+
+difflist = {"1": "pussy",
+"2": "easier",
+"3": "easy",
+"4": "medium",
+"5": "hard",
+"6": "very hard",
+"7": "nightmare",
+"8": "impossible",
+"9": "endless"
+}
+
 stats = {"hp": health, "pnt": points, "lvl": level, "xp": exp, "req": expreq, "atk": attack, "def": defense, "spd": speed}
 
 words = ["one","into","a","seat","flip","not","unset","nail","joke","laugh","enjoy","old","eye","file","bring","love","lie","sprawl","shook","rather","involved","time","right","there","left","tell","camera","before","more","over","flee","stand","rush","friend","address","of","emergecy","guy","got","down","couple", "hunt", "run","without","help","trace","old","year","state","car", "hands", "arrived", "anyone", "on", "this", "fall","high","try","burn","toss","lap","cant","did","do","too","to","two","flow","tan","brain","glove","county","had","come","here","room","work"]
 
 if "oneturnfight" not in os.listdir("/etc"):
 	os.makedirs(conf_path, exist_ok=True)
+if "local_scores.conf" not in os.listdir(conf_path):
+	with open(f"{conf_path}/local_scores.conf", "w") as file:
+		file.write("")
 if "local_player.conf" not in os.listdir(conf_path):
 	with open(f"{conf_path}/local_player.conf", "w") as file:
 		for name, val in stats.items():
@@ -61,10 +78,21 @@ else:
 def main():
 	print("Welcome to One Turn Fight, either fight locally or fight online!\n\nType 'login' to go online, otherwise type 'help' for more inormation")
 	while True:
-		inp = input(">>> ")
+		inp = input("(main menu) >>> ")
 		xcute = cmds.get(inp)
 		if xcute:
-			xcute()
+			if "fight" in inp:
+				for val, name in difflist.items():
+					print(f"{val}. {name}")
+				inp = input("Select difficulty (press enter for default): ")
+				if inp not in "1234567890":
+					continue
+				if not inp:
+					xcute(None)
+				else:
+					xcute(difflist[inp.strip()])
+			else:
+				xcute()
 
 
 def online():
@@ -81,7 +109,7 @@ def online():
 		firstmsg = server.recv(48).decode("utf-8")
 		print(firstmsg)
 		while True:
-			inp = input(">>> ")
+			inp = input("(online) >>> ")
 			server.send(inp.encode("utf-8"))
 			srvresponse = server.recv(1024).decode("utf-8")
 			print(srvresponse)
@@ -102,22 +130,73 @@ def timer(count):
 		cache += 1
 	done = 1
 
-def localfight():
+def localfight(diff):
 	global count
 	
-	cachehp = stats["hp"]
+	finalright = 0
+	finalwrong = 0
 	
-	ename = "john doe"
+	cachehp = stats["hp"]
+	bonus = 0
 	ehealth = 0
 	eattack = 0
 	edefense = 0
 	elevel = 0
-	
-	hostile = {"name": ename,"hp": ehealth, "atk": eattack, "def": edefense, "lvl": elevel}
-	hostile["hp"] = random.randint(int(stats["hp"]) - 5, int(stats["hp"]) + 10)
-	hostile["atk"] = random.randint(int(stats["atk"]) - 3, int(stats["atk"]) + 5)
-	hostile["def"] = random.randint(int(stats["def"]) - 3, int(stats["def"]) + 5)
-	hostile["lvl"] = random.randint(int(stats["lvl"]) - 3, int(stats["lvl"]) + 5)
+	difftime = 0
+	diffcount = 0
+	if diff == "pussy":
+		difftime = -14
+		diffcount = 3
+		ehealth -= 10
+		eattack -= 5
+		edefense -= 5
+	elif diff == "easier":
+		difftime = -12
+		diffcount = 2
+		ehealth -= 7
+		eattack -= 3
+		edefense -= 3
+	elif diff == "easy":
+		difftime = -9
+		diffcount = 1
+		ehealth -= 5
+		eattack -= 1
+		edefense -= 1
+	elif diff == "medium":
+		difftime = -4
+		ehealth += 3
+		eattack += 2
+		edefense += 2
+	elif diff == "hard":
+		difftime = 2
+		ehealth += 5
+		eattack += 3
+		edefense += 3
+	elif diff == "very hard":
+		difftime = 5
+		diffcount = -1
+		ehealth += 10
+		eattack += 4
+		edefense += 4
+	elif diff == "nightmare":
+		difftime = 7
+		diffcount = -1
+		ehealth += 15
+		eattack += 5
+		edefense += 5
+	elif diff == "impossible":
+		difftime = 9
+		diffcount = -2
+		ehealth += 25
+		eattack += 7
+		edefense += 7
+	ename = botnames[random.randint(0, len(botnames) - 1)]
+	hostile = {"name": ename,"hp": ehealth, "atk": eattack, "def": edefense, "lvl": elevel, "diff": difftime, "bonus": bonus}
+	hostile["hp"] += random.randint(int(stats["hp"]) - 5, int(stats["hp"]) + 20)
+	hostile["atk"] += random.randint(int(stats["atk"]) - 3, int(stats["atk"]) + 5)
+	hostile["def"] += random.randint(int(stats["def"]) - 3, int(stats["def"]) + 5)
+	hostile["lvl"] += random.randint(int(stats["lvl"]) - 3, int(stats["lvl"]) + 5)
+	hostile["bonus"] += random.randint(0, int(hostile["lvl"]) + 15)
 	
 	if hostile["atk"] <= 0:
 		hostile["atk"] = 1
@@ -125,8 +204,13 @@ def localfight():
 		hostile["def"] = 0
 	if hostile["lvl"] <= 0:
 		hostile["lvl"] = 0
-	for nm, val in hostile.items():
-		print(val)
+	if diff == "endless":
+		ename = "god"
+		hostile["atk"] = 1
+		hostile["def"] = 0
+		hostile["lvl"] = 0
+		hostile["hp"] = 10000000000
+	print(f"You are facing: {ename}")
 	time.sleep(1)
 	print("Ready?")
 	time.sleep(1)
@@ -139,31 +223,37 @@ def localfight():
 	while True:
 		spellme = words[random.randint(0, len(words) - 1)]
 		score = 0
-		count = random.randint(0,7 + (stats["spd"] // 10))
+		count = random.randint(0,4 + diffcount + (stats["spd"] // 10))
 		time_thread = threading.Thread(target=timer, args=(count,))
 		time_thread.start()
 		hostile_thread = threading.Thread(target=hostilefight, args=(hostile,))
 		hostile_thread.start()
 		while not done:
 			spellme = words[random.randint(0, len(words) - 1)]
-			inp = input(f"Word: {spellme}\n>>> ")
+			inp = input(f"Word: {spellme}\n(fight) >>> ")
+			if auto:
+				inp = spellme
 			if inp == spellme:
 				score += 1
+				finalright += 1
+			else:
+				finalwrong += 1
 		time_thread.join()
+		hostile["bonus"] += score
 		hostile["hp"] -= score + int(stats["atk"])
 		if hostile["hp"] <= 0:
 			hostile_thread.join()
 			win = 1
 			print(f"{hostile['name']} was defeated, you had {stats['hp']} health left")
-			after_fight(win, hostile, cachehp)
+			after_fight(win, hostile, cachehp, finalright, finalwrong)
 			break
 		elif stats["hp"] <= 0:
 			hostile_thread.join()
 			win = 0
-			print("you were defeated")
-			after_fight(win, None, cachehp)
+			print(f"you were defeated. Tallies:\nWords right: {finalright}\nWords wrong: {finalwrong}\nHealth: {stats['hp']}\nHostiles health: {hostile['hp']}")
+			after_fight(win, None, cachehp, finalright, finalwrong)
 			break
-		print(f"{hostile['name']} took f{score + int(stats['atk'])} damage. You hace {stats['hp']} health")
+		print(f"{hostile['name']} took f{score + int(stats['atk'])} damage. You have {stats['hp']} health")
 
 def hostilefight(hostile):
 	while True:
@@ -178,26 +268,46 @@ def hostilefight(hostile):
 			for i in spellme:
 				if hostile["hp"] <= 0 or stats["hp"] <= 0:
 					break
-				waittime = random.randint(12, 18) / 10
+				if hostile["diff"] < 0:
+					waittime = random.randint(12 - random.randint(hostile["diff"], 0), 29 - random.randint(hostile["diff"], 0)) / 10
+				else:
+					waittime = random.randint(12 - random.randint(0, hostile["diff"]), 22 - random.randint(0, hostile["diff"])) / 10
+
 				time.sleep(waittime)
 			if hostile["hp"] <= 0 or stats["hp"] <= 0:
 				break
 			score += 1
 			if random.randint(0,1) == 1:
 				score -= 1
+			hostile["bonus"] += score
 			stats["hp"] -= score + int(hostile["atk"])
 		
-def after_fight(status, reward, cachehp):
+def after_fight(status, reward, cachehp, right, wrong):
 	cache = 0
 	stats["hp"] = cachehp
 	if status:
 		for trash, val in reward.items():
-			if trash == "name" or trash == "hp":
+			if trash == "name" or trash == "diff":
 				continue
+			if val < 0:
+				val *= -1
 			cache += val
 		stats["xp"] += cache
 		savestat()
 		print(f"youve gained {cache} exp")
+	with open(f"{conf_path}/local_scores.conf", "r") as file:
+		data = file.readlines()
+	with open(f"{conf_path}/local_scores.conf", "w") as file:
+		for item in data:
+			if "r" in item:
+				item = item.strip("r")
+				right = right + int(item)
+			elif "w" in item:
+				item = item.strip("w")
+				wrong = wrong + int(item)
+		file.write(f"r{right}\nw{wrong}")
+	
+		
 		
 def savestat():
 	if stats["xp"] >= stats["req"]:
@@ -214,8 +324,51 @@ def savestat():
 		print("Leveled up!")
 	with open(f"{conf_path}/local_player.conf", "w") as file:
 		for name, val in stats.items():
-			file.write(f"{name}={str(val)}\n".encode("utf-8"))
-		
-cmds = {"fight": localfight, "login": online}
+			file.write(f"{name}={str(val)}\n")
+def getscore():
+	with open(f"{conf_path}/local_scores.conf", "r") as file:
+		data = file.readlines()
+		if not data:
+			return
+		for item in data:
+			if "r" in item:
+				r = item.strip('r').strip('\n')
+				print(f"Right: {r}")
+			elif "w" in item:
+				w = item.strip('w')
+				print(f"Wrong: {w}")
+		numscore = (int(r) / int(w)) * 10000
+		print(f"Rating: {numscore // 1}")
+		if numscore < 25000:
+			score = "Too low to be measured"
+		if numscore > 25000:
+			score = "E"
+		if numscore > 35000:
+			score = "F"
+		if numscore > 50000:
+			score = "D"
+		if numscore > 850000:
+			score = "C"
+		if numscore > 100000:
+			score = "B"
+		if numscore > 150000:
+			score = "A"
+		if numscore > 200000:
+			score = "S"
+		if numscore > 300000:
+			score = "Z"
+		if numscore > 500000:
+			score = "God"
+		if int(r) < 100:
+			score = "Too low to be measured"
+		print(f"Grade: {score}")
+
+def helpy():
+	for name, val in cmds.items():
+		print(name)
+def getstat():
+	for name, val in stats.items():
+		print(f"{name}: {val}")
+cmds = {"fight": localfight, "login": online, "help": helpy, "score": getscore, "stats": getstat}
 
 main()
